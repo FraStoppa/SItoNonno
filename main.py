3c34a454e8a398e96e6f0e90bb52d16485a13e11
FraStoppa <frastoppa23@gmail.com>
2022-06-09 15:48:53 +0200

    ho creato questa figata!

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ def home_page():
     return "<h1>Hello world!!</h1>"
 
 
+@app.route("/contatti")
+def contact():
+    return "<h2>Questa è la pagina dei contatti</h2>"
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
