67ac56bee64f09051f7f44c2175996a7d0b5cabd
FraStoppa <frastoppa23@gmail.com>
2022-06-09 15:57:33 +0200

    inserito l'About us (prototipo)

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def home_page():
     return "<h1>Hello world!!</h1>"
 
 
+@app.route("/about")
+def about_us():
+    return "<p>Questi siamo noi</p>"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
